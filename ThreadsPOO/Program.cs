a89351768b56b6414f6b52a237bd7fbbacd4660b
RaulLH <raullopezhernando@gmail.com>
2020-01-12 10:02:30 +0100

    Agregar archivos de proyecto.

---
+++ ThreadsPOO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace ThreadsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tareas Object1 = new Tareas("Hilo_1",8);
+            //Tareas Object2 = new Tareas("Hilo_2", 16);
+
+            //// Ejecutamos los metodos sobre neustros hilos
+            //Thread hilo1 = new Thread(Object1.Tarea1);
+            //Thread hilo2 = new Thread(Object2.Tarea2);
+            ////Comienzo de los hilos
+            //hilo1.Start();
+            //hilo2.Start();
+
+            ArrayList listTareas = new ArrayList();
+            listTareas.Add("Hilo1");
+            listTareas.Add("Hilo2");
+            listTareas.Add("Hilo3");
+
+            ArrayList listSecond = new ArrayList();
+            listSecond.Add(8);
+            listSecond.Add(16);
+            listSecond.Add(24);
+
+            for (int i = 0; i < listTareas.Count; i++) 
+            {
+                object tarea = listTareas[i];
+                string tareas = Convert.ToString(tarea);
+                object secs = listSecond[i];
+                int sec = Convert.ToInt32(secs);
+                Tareas Obj1 = new Tareas(tareas, sec);
+                Obj1.Tarea1();
+            
+            }
+        }
+    }
+}
---
+++ ThreadsPOO/Tareas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadsPOO
+{
+    public class Tareas
+    {
+        private String name;
+        private int second;
+
+        // Constructor
+        public Tareas(String name, int second) 
+        {
+            this.name = name;
+            this.second = second; 
+        
+        }
+
+        public void Tarea1() 
+        {
+            Console.WriteLine(name + "Esta tarea solo tendra" + second + "segundos.");
+            Thread.Sleep(second * 1000); //Thread.Sleep. Suspende el subproceso actual durante un periodo de tiempo especificado.
+            Console.WriteLine("Tarea terminada " + name);
+
+        }
+
+        public void Tarea2()
+        {
+            Console.WriteLine(name + "Esta tarea solo tendra" + second + "segundos.");
+            Thread.Sleep(second * 1000); //Thread.Sleep. Suspende el subproceso actual durante un periodo de tiempo especificado.
+            Console.WriteLine("Tarea terminada " + name);
+
+        }
+
+    }
+}
